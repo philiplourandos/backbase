a25f286d80a68714c06e686d939c2a8046b66bb5
Philip Lourandos <philiplourandos@frei.one>
2019-09-04 18:09:13 +0200

    * Adding test cases

---
+++ mancala-game/src/test/java/com/backbase/mancala/game/GameTest.java
@@ -0,0 +1,22 @@
+package com.backbase.mancala.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class GameTest {
+    @Test
+    public void when_invalidPositionGiven_thenDoNotPlayTurn() {
+        fail();
+    }
+    
+    @Test
+    public void when_houseUsedAsStartingPoint_thenDoNotPlayTurn() {
+        fail();
+    }
+    
+    @Test
+    public void when_emptyPitSelectedEmpty_thenDoNotPlayTurn() {
+        fail();
+    }
+}
