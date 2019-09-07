e581d52320f7bf434201a67d9b0b06161adb9e08
Philip Lourandos <philiplourandos@frei.one>
2019-09-07 23:42:24 +0200

    * Added test case to process input file

--- mancala-game/src/test/java/com/backbase/mancala/game/GameTest.java
+++ mancala-game/src/test/java/com/backbase/mancala/game/GameTest.java
@@ -1,14 +1,29 @@
 package com.backbase.mancala.game;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.core.io.ClassPathResource;
 
-public class GameTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GameTest {    
+    private static final Logger LOG = LogManager.getLogger(GameTest.class);
+
     private static final int DEFAULT_STONES = 6;
+    private static final int INPUT_START_PIT_INDEX = 14;
+    private static final int INPUT_CURRENT_PLAYER_INDEX = 15;
+    
+    private final Game successfulGame = new Game(1, DEFAULT_STONES);
     
     @Test
     public void when_invalidPositionGiven_thenDoNotPlayTurn() {
@@ -29,11 +44,36 @@ public class GameTest {
     
     @Test
     public void when_emptyPitSelectedEmpty_thenDoNotPlayTurn() {
-        fail();
+        final Game mancala = new Game(2, 0);
+        
+        boolean turnPlayed = mancala.playTurn(1);
+        assertFalse(turnPlayed);
     }
     
     @Test
-    public void when_gamePlayedToConclusion_thenEndWithExpectedOutCome() {
-        fail();
+    public void when_gamePlayedToConclusion_thenEndWithExpectedOutCome() throws Exception {
+        final ClassPathResource testFile = new ClassPathResource("full-game.csv");
+
+        final Path filePath = Paths.get(testFile.getURI());
+        
+        Files.readAllLines(filePath, Charset.forName("UTF-8")).stream()
+                .skip(2)
+                .forEach(this::processInput);
+    }
+    
+    private void processInput(final String gameInput) {
+        final String[] values = gameInput.split(",");
+
+        assertEquals(Players.valueOf(values[INPUT_CURRENT_PLAYER_INDEX]), successfulGame.getCurrentPlayer());
+        final boolean turnPlayed = successfulGame.playTurn(Integer.valueOf(values[INPUT_START_PIT_INDEX]));
+        assertTrue(turnPlayed);
+        
+        LOG.info("{}", successfulGame);
+        
+        final Map<Integer, GameAction> board = successfulGame.getBoard();
+        
+        for(int index = 0; index < 14; index++) {
+            assertEquals(Integer.valueOf(values[index]), Integer.valueOf(board.get(index + 1).countStones()));
+        }
     }
 }
