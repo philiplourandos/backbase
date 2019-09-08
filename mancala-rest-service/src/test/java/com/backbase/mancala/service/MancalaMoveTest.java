235428b9e31cd5afb4d9c2ab9b7d24c0a7ea598d
Philip Lourandos <philiplourandos@frei.one>
2019-09-08 11:30:23 +0200

    * Implemented successful test case

--- mancala-rest-service/src/test/java/com/backbase/mancala/service/MancalaMoveTest.java
+++ mancala-rest-service/src/test/java/com/backbase/mancala/service/MancalaMoveTest.java
@@ -1,8 +1,15 @@
 package com.backbase.mancala.service;
 
 import com.backbase.mancala.service.domain.CreateGameResponse;
+import com.backbase.mancala.service.domain.MoveResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,9 +20,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -23,12 +32,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 public class MancalaMoveTest {
     private static final String API = "/games/%d/pits/%d";
+
+    private static final Integer[] EXPECTED_VALUES = {0, 7, 7, 7, 7, 7, 1, 6, 6, 6, 6, 6, 6, 0};
+    private static final Map<Integer, Integer> EXPECTED_STATUS = new TreeMap<>();
+    
     @Autowired
     private MockMvc mvc;
     
     private Integer gameId;
     
     private final ObjectMapper jsonMapper = new ObjectMapper();
+    
+    @BeforeAll
+    public static void setup() {
+        final AtomicInteger index = new AtomicInteger(1);
+
+        Arrays.stream(EXPECTED_VALUES).forEach(a -> EXPECTED_STATUS.put(index.getAndIncrement(), a));
+    }
     
     @BeforeEach
     public void createGame() throws Exception {
@@ -43,7 +63,18 @@ public class MancalaMoveTest {
     
     @Test
     public void when_validMoveMade_thenRespondSuccessfully() throws Exception {
-        fail();
+        final MvcResult result = mvc.perform(put(String.format(API, gameId, 1)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.url").isNotEmpty())
+                .andExpect(jsonPath("$.status").isNotEmpty())
+                .andReturn();
+        
+        final MoveResponse responseValue = jsonMapper.readValue(
+                result.getResponse().getContentAsString(), MoveResponse.class);
+        assertNotNull(responseValue);
+        
+        assertEquals(EXPECTED_STATUS, responseValue.getStatus());
     }
     
     @Test
