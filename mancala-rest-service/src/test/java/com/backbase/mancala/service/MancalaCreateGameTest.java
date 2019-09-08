71a20d38d94395ffbe05b933eb4263b3f69bfdac
Philip Lourandos <philiplourandos@frei.one>
2019-09-08 08:43:26 +0200

    * Test case for create game

---
+++ mancala-rest-service/src/test/java/com/backbase/mancala/service/MancalaCreateGameTest.java
@@ -0,0 +1,32 @@
+package com.backbase.mancala.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(GameEndpoint.class)
+@ActiveProfiles("test")
+public class MancalaCreateGameTest {
+    @Autowired
+    private MockMvc mvc;
+    
+    @Test
+    public void when_validCallToCreateGameCalled_thenCreateGame() throws Exception {
+        mvc.perform(post("/games"))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.uri").value("http://localhost:8080/games/1"));
+    }
+}
